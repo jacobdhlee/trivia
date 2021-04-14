5590616a91d57db58d4d70d2ff97c1420565f27a
jacobdhlee <jacobdhlee@gmail.com>
2021-04-14 01:29:05 -0400

    frontend add route to homepage when post success

--- projects/02_trivia_api/starter/frontend/src/components/FormView.js
+++ projects/02_trivia_api/starter/frontend/src/components/FormView.js
@@ -50,6 +50,7 @@ class FormView extends Component {
       crossDomain: true,
       success: (result) => {
         document.getElementById("add-question-form").reset();
+        this.props.history.push('/')
         return;
       },
       error: (error) => {
